878a57c000a4f68ee7997833af4d630a73d9cd34
Oscar Saraza <oscarsaraza@gmail.com>
2022-12-29 08:36:15 -0500

    Keyboard component receive physical keyboard events
    - Updates #1

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
 App name: "CAT MATH"
 
-- Measure answers time
-- Add sounds
-  - Correct answer
-  - Incorrect answer
-- Start screen
-  - Cat thinking on math
-  - "Start" button
 - Design graphics
   - Color palette
   - Typography
 - 10 correct answers: show "keep going"
-- User accounts system
-  - Generate random user name and assign a numeric password
-  - Save practice data
-    - date
-    - completed
-    - total time
-  - Remove unused accounts after 10 days of inactivity
--- src/routes/keyboard.svelte
+++ src/routes/keyboard.svelte
@@ -4,11 +4,18 @@
 	const dispatch = createEventDispatcher();
 	export let value: string = '';
 
-	function onButtonClick(number: number) {
-		return () => {
-			if (value.length <= 2) value = `${value}${number}`;
-		};
+	function addNumberToAnswer(number: number) {
+		if (value.length <= 2) value = `${value}${number}`;
 	}
+
+	const onNumberClick = (number: number) => () => addNumberToAnswer(number);
+
+	const handleKeydown = ({ key }: KeyboardEvent) => {
+		const number = parseInt(key);
+		if (!Number.isNaN(number)) addNumberToAnswer(number);
+		else if (key === 'Enter') onOkButtonClick();
+		else if (key === 'Backspace') onDelButtonClick();
+	};
 
 	function onOkButtonClick() {
 		dispatch('okClick');
@@ -19,18 +26,20 @@
 	}
 </script>
 
+<svelte:window on:keydown={handleKeydown} />
+
 <div id="keyboard">
-	<button class="btn" on:click={onButtonClick(1)}>1</button>
-	<button class="btn" on:click={onButtonClick(2)}>2</button>
-	<button class="btn" on:click={onButtonClick(3)}>3</button>
-	<button class="btn" on:click={onButtonClick(4)}>4</button>
-	<button class="btn" on:click={onButtonClick(5)}>5</button>
-	<button class="btn" on:click={onButtonClick(6)}>6</button>
-	<button class="btn" on:click={onButtonClick(7)}>7</button>
-	<button class="btn" on:click={onButtonClick(8)}>8</button>
-	<button class="btn" on:click={onButtonClick(9)}>9</button>
+	<button class="btn" on:click={onNumberClick(1)}>1</button>
+	<button class="btn" on:click={onNumberClick(2)}>2</button>
+	<button class="btn" on:click={onNumberClick(3)}>3</button>
+	<button class="btn" on:click={onNumberClick(4)}>4</button>
+	<button class="btn" on:click={onNumberClick(5)}>5</button>
+	<button class="btn" on:click={onNumberClick(6)}>6</button>
+	<button class="btn" on:click={onNumberClick(7)}>7</button>
+	<button class="btn" on:click={onNumberClick(8)}>8</button>
+	<button class="btn" on:click={onNumberClick(9)}>9</button>
 	<button class="btn" on:click={onOkButtonClick}>✔</button>
-	<button class="btn" on:click={onButtonClick(0)}>0</button>
+	<button class="btn" on:click={onNumberClick(0)}>0</button>
 	<button class="btn" on:click={onDelButtonClick}>←</button>
 </div>
 
